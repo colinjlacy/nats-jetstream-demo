7d9fc96c7fc51c3677cae67cb3edefa65ec0ddc8
Colin Lacy <colacy@cisco.com>
2025-05-29 20:10:04 -0400

    reverts stream maximums, removes de-dupe demo code

--- services/requester/main.py
+++ services/requester/main.py
@@ -49,16 +49,10 @@ async def main():
                     try:
                         if result > 1:
                             print(f"Result is significant: {result}")
-                            await nc.publish("answers.significant", json.dumps(answer).encode('utf-8'), None, {"Nats-Msg-Id": f"{subject}-response-{i}"})
-                            if os.environ.get("DUPLICATE", "false") == "true":
-                                await asyncio.sleep(1)  # Simulate a delay for duplicate processing
-                                await nc.publish("answers.significant", json.dumps(answer).encode('utf-8'), None, {"Nats-Msg-Id": f"{subject}-response-{i}"})
+                            await nc.publish("answers.significant", json.dumps(answer).encode('utf-8'))
                         else:
                             print(f"Result is not significant: {result}")
-                            await nc.publish("answers.throwaway", json.dumps(answer).encode('utf-8'), None, {"Nats-Msg-Id": f"{subject}-response-{i}"})
-                            if os.environ.get("DUPLICATE", "false") == "true":
-                                await asyncio.sleep(1)
-                                await nc.publish("answers.significant", json.dumps(answer).encode('utf-8'), None, {"Nats-Msg-Id": f"{subject}-response-{i}"})
+                            await nc.publish("answers.throwaway", json.dumps(answer).encode('utf-8'))
                     except Exception as e:
                         print(f"Error publishing result message: {e}")
             except Exception as e:
