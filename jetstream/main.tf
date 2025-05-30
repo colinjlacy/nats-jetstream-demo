1da1bd679aba1e23c4e28e885a47c6920c7c2208
Colin Lacy <colacy@cisco.com>
2025-05-30 04:58:22 -0400

    moves tofu vars and provider block

--- jetstream/main.tf
+++ jetstream/main.tf
@@ -1,29 +1,3 @@
-variable "nats_servers" {
-  description = "The NATS server address"
-  type        = string
-}
-variable "nats_tls_ca_file" {
-  description = "The CA file for NATS TLS"
-  type        = string
-}
-variable "nats_tls_cert_file" {
-  description = "The certificate file for NATS TLS"
-  type        = string
-}
-variable "nats_tls_key_file" {
-  description = "The key file for NATS TLS"
-  type        = string
-}
-
-provider "jetstream" {
-  # the domain of your cloud LB
-  servers     = var.nats_servers
-  tls {
-    ca_file = var.nats_tls_ca_file
-    cert_file = var.nats_tls_cert_file
-    key_file  = var.nats_tls_key_file
-  }
-}
 
 resource "jetstream_stream" "answers" {
   name     = "answers"
--- jetstream/providers.tf
+++ jetstream/providers.tf
@@ -1,3 +1,21 @@
+
+variable "nats_servers" {
+  description = "The NATS server address"
+  type        = string
+}
+variable "nats_tls_ca_file" {
+  description = "The CA file for NATS TLS"
+  type        = string
+}
+variable "nats_tls_cert_file" {
+  description = "The certificate file for NATS TLS"
+  type        = string
+}
+variable "nats_tls_key_file" {
+  description = "The key file for NATS TLS"
+  type        = string
+}
+
 terraform {
   required_providers {
     jetstream = {
@@ -7,4 +25,14 @@ terraform {
   }
 
   required_version = ">= 1.8.0"
+}
+
+provider "jetstream" {
+  # the domain of your cloud LB
+  servers     = var.nats_servers
+  tls {
+    ca_file = var.nats_tls_ca_file
+    cert_file = var.nats_tls_cert_file
+    key_file  = var.nats_tls_key_file
+  }
 }
